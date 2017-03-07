17248549874d22dbc3bc427e36ae4d84774b6ada
EdsonVelazquez <many_edson@hotmail.com>
2017-03-07 13:51:52 -0600

    Update ejemplo-camara.js

--- ejemplo-camara.js
+++ ejemplo-camara.js
@@ -1,6 +1,6 @@
 var campoVision     = 45; // grados
 var relacionAspecto = window.innerWidth / window.innerHeight;
-var planoCercanp    = 1;
+var planoCercano    = 1;
 var planoLejano     = 1000;
 
 var camara = new THREE.PerspectiveCamera( campoVision, relacionAspecto, planoCercano, planoLejano );
