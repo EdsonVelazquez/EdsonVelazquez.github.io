9f063cbaa0c56c081a31e29b83da03386e82ecb2
EdsonVelazquez <many_edson@hotmail.com>
2017-03-16 14:04:49 -0600

    Create tortuga.js

---
+++ tortuga.js
@@ -0,0 +1,52 @@
+var Cabeza = new THREE.SphereGeometry( 1 );
+
+var Caparazon = new THREE.SphereGeometry( 2, 8, 6, 0, Math.PI );
+
+var PataFI = new THREE.CylinderGeometry(.6, .4, .8);
+var PataFD = new THREE.CylinderGeometry(.6, .4, .8);
+var PataTI = new THREE.CylinderGeometry(.6, .4, .8);
+var PataTD = new THREE.CylinderGeometry(.6, .4, .8);
+
+Cabeza.translate(1.8,1,0);
+
+PataTI.translate(-0.75, -0.4, -0.75);
+PataTD.translate(-0.75, -0.4,  0.75);
+PataFI.translate( 0.75, -0.4, -0.75);
+PataFD.translate( 0.75, -0.4,  0.75);
+
+var CabezaM = new THREE.Mesh(Cabeza);
+
+var CaparazonM = new THREE.Mesh(Caparazon);
+
+var PataTIM = new THREE.Mesh(PataTI);
+var PataTDM = new THREE.Mesh(PataTD);
+var PataFIM = new THREE.Mesh(PataFI);
+var PataFDM = new THREE.Mesh(PataFD);
+
+var Tortuga = new THREE.Geometry();
+
+Tortuga.merge(CabezaM.geometry, CabezaM.matrix);
+
+Tortuga.merge(CaparazonM.geometry, CaparazonM.matrix);
+
+Tortuga.merge(PataTIM.geometry, PataTIM.matrix);
+Tortuga.merge(PataTDM.geometry, PataTDM.matrix);
+Tortuga.merge(PataFIM.geometry, PataFIM.matrix);
+Tortuga.merge(PataFDM.geometry, PataFDM.matrix);
+
+var material = new THREE.MeshNormalMaterial();
+var TortugaM = new THREE.Mesh(Tortuga, material);
+TortugaM.rotateY(Math.PI/2);
+//TortugaM.rotateY(Math.PI/4);
+//TortugaM.rotateX(Math.PI/8);
+
+var escena = new THREE.Scene();
+escena.add(TortugaM);
+
+var camara = new THREE.PerspectiveCamera();
+camara.position.z = 50;
+
+renderizador = new THREE.WebGLRenderer();
+renderizador.setSize( window.innerHeight*.95, window.innerHeight*.95 );
+document.body.appendChild( renderizador.domElement );
+renderizador.render( escena, camara );
