d15fde86dfa247b0d7e28a79a78a77081fa680ba
EdsonVelazquez <many_edson@hotmail.com>
2017-03-20 19:33:11 -0600

    Update practicaperspectiva.js

--- practicaperspectiva.js
+++ practicaperspectiva.js
@@ -265,10 +265,10 @@ camara.right = window.innerWidth / 2;
 camara.top = window.innerHeight / 2;
 camara.bottom = window.innerHeight / -2;
 camara.near = 0.1;
-camara.far = 1000;
+camara.far = 100;
 camara.updateProjectionMatrix();
 
-camara.position.z = 10;
+camara.position.z = 0.1;
 
 var renderizador = new THREE.WebGLRenderer();
 renderizador.setSize(window.innerWidth,window.innerHeight);
