91d55a92ea030447f8a014682aa5bd4ede3e95ed
EdsonVelazquez <many_edson@hotmail.com>
2017-03-20 19:37:12 -0600

    Update practicaperspectiva.js

--- practicaperspectiva.js
+++ practicaperspectiva.js
@@ -265,7 +265,7 @@ camara.right = window.innerWidth / 2;
 camara.top = window.innerHeight / 2;
 camara.bottom = window.innerHeight / -2;
 camara.near = 0.1;
-camara.far = 100;
+camara.far = 10;
 camara.updateProjectionMatrix();
 
 camara.position.z = 0.1;
