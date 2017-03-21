b2cc9404a26c17d8d3e5d46c65a6175b57651988
EdsonVelazquez <many_edson@hotmail.com>
2017-03-21 14:23:06 -0600

    Update ejemplo-animacion.js

--- ejemplo-animacion.js
+++ ejemplo-animacion.js
@@ -3,7 +3,7 @@ function setup() {
   escena = new THREE.Scene();
   camara = new THREE.PerspectiveCamera( 75,
                                         window.innerWidth / window.innerHeight,
-                                        0.1;
+                                        0.1,
                                         1000 );
 
   camara.position.z = 5;
