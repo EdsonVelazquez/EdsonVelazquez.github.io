0f0395a4ec59fefec92dc862373a6c412d117234
EdsonVelazquez <many_edson@hotmail.com>
2017-03-20 18:23:18 -0600

    Update practicaperspectiva.js

--- practicaperspectiva.js
+++ practicaperspectiva.js
@@ -232,6 +232,8 @@ escena.add( cubo25 );
 escena.add(piezaMalla);
 escena.add(TortugaM);
 
+/* Perspective Camera
+
 var campoVision = 60;
 var relacionAspecto = window.innerWidth/window.innerHeight;
 var planoCercano = 1;
@@ -253,6 +255,20 @@ camara.position.z = 25;
 camara.position.y = 25;
 camara.rotateY(-Math.PI/4);
 camara.rotateX(-Math.PI/4);
+
+*/
+
+var camara = new THREE.OrthographicCamera();
+
+camara.left = window.innerWidth / -2;
+camara.right = window.innerWidth / 2;
+camara.top = window.innerHeight / 2;
+camara.bottom = window.innerHeight / -2;
+camara.near = 0.1;
+camara.far = 100;
+camara.updateProjectionMatrix();
+
+camara.position.z = 30;
 
 var renderizador = new THREE.WebGLRenderer();
 renderizador.setSize(window.innerWidth,window.innerHeight);
